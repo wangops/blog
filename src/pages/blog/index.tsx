444117f34b6b11bbe5887b6f0393245075398334
wanghexiang <337192133@qq.com>
2024-10-10 11:15:12 +0800

    修复Bug

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,6 +1,29 @@
+
+import style from "@/style/blog.module.css"
 export default function BlogIndex (){
     return (
-        <div>
+        <div className={style.blog_container}>
+            <div className={style.blog_options} style={{"background-color":"red"}}>
+                <p className={style.blog_options_title}>Vue</p>
+            </div>
+            <div className={style.blog_options}>
+                <p className={style.blog_options_title}>JS/TS</p>
+            </div>
+            <div className={style.blog_options}>
+                <p className={style.blog_options_title}>Web</p>
+            </div>
+            <div className={style.blog_options}>
+                <p className={style.blog_options_title}>CSS</p>
+            </div>
+            <div className={style.blog_options}>
+                <p className={style.blog_options_title}>iOS</p>
+            </div>
+            <div className={style.blog_options}>
+                <p className={style.blog_options_title}>Algorithm</p>
+            </div>
+            <div className={style.blog_options}>
+                <p className={style.blog_options_title}>HTTP</p>
+            </div>
         </div>
     );
 }
